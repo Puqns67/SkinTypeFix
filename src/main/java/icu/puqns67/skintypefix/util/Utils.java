3f191d7c302dc7db45089e60b53ed1253966566f
Puqns67 <me@puqns67.icu>
2024-07-09 00:35:14 +0800

    chore: some small improvements

--- src/main/java/icu/puqns67/skintypefix/util/Utils.java
+++ src/main/java/icu/puqns67/skintypefix/util/Utils.java
@@ -1,12 +1,11 @@
 package icu.puqns67.skintypefix.util;
 
+import net.minecraft.Util;
 import net.minecraft.client.resources.PlayerSkin;
 
 import java.util.UUID;
 
 public class Utils {
-	private static final UUID INVALID_UUID = new UUID(0, 0);
-
 	public static PlayerSkin.Model reverseModelType(PlayerSkin.Model type) {
 		return switch (type) {
 			case SLIM -> PlayerSkin.Model.WIDE;
@@ -15,6 +14,6 @@ public class Utils {
 	}
 
 	public static boolean isInvalidUUID(UUID uuid) {
-		return INVALID_UUID.equals(uuid);
+		return Util.NIL_UUID.equals(uuid);
 	}
 }
