db6d1383f7968fec70dab7c56a95a19eed12aabc
Puqns67 <me@puqns67.icu>
2024-08-22 00:49:55 +0800

    release: fabric@0.1.0-beta.8+mc1.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ fun getVersionType(version: String): String {
 
 plugins {
 	id("java")
-	id("fabric-loom") version "1.7.2"
+	id("fabric-loom") version "1.7.3"
 	id("com.modrinth.minotaur") version "2.8.7"
 	id("idea")
 }
